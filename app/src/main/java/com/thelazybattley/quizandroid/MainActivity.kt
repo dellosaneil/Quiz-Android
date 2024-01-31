ebf1ebb8169ea7dc2fb49fa01712c3179c01878f
neiljedricdellosa <neil.d@poplarhomes.com>
2024-01-31 21:16:55 +0800

    clean up main activity

--- app/src/main/java/com/thelazybattley/quizandroid/MainActivity.kt
+++ app/src/main/java/com/thelazybattley/quizandroid/MainActivity.kt
@@ -3,32 +3,15 @@ package com.thelazybattley.quizandroid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.lifecycle.lifecycleScope
-import com.thelazybattley.domain.network.usecase.AddQuestionUseCase
 import com.thelazybattley.quizandroid.ui.theme.QuizAndroidTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    @Inject
-    lateinit var useCase: AddQuestionUseCase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        lifecycleScope.launch {
-            println(
-                "Test: ${
-                    useCase(
-                        question = "Testing?",
-                        answer = "yes",
-                        choices = listOf("1", "2", "3", "4")
-                    )
-                }"
-            )
-        }
         setContent {
             QuizAndroidTheme {
 
